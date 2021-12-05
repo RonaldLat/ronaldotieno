f65d14807a3e98b9e8c403725a9e0c5d3bc2c83f
Ronald <ronnlats@gmail.com>
2021-12-05 22:25:20 +0300

    extra padding for blog layout footer

--- layouts/bloglayout.vue
+++ layouts/bloglayout.vue
@@ -1,10 +1,10 @@
 <template>
-  <main class=" min-h-screen relative pb-96 bg-gray-300 w-full  overflow-x-hidden ">
+  <main class="  min-h-screen relative pb-96 bg-gray-300 w-full  overflow-x-hidden ">
     <TheBlogHeader />
   <Nuxt />
       <div class="absolute w-full  left-0 bottom-0">
 
-  <footer class="w-full px-2 sm:px-4 py-16 lg:px-8    md:px-24 lg:px-8 lg:py-20 text-gray-200 bg-gray-900 flex flex-col gap-2 ">
+  <footer class="w-full px-4 sm:px-4 py-16 lg:px-8    md:px-24 lg:px-8 lg:py-20 text-gray-200 bg-gray-900 flex flex-col gap-2 ">
     <p class="uppercase text-gray-300 text-sm font-bold tracking-wide">
       subscribe
     </p>
