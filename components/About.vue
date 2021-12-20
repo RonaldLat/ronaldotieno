e9b873f8fb17815b3bd242e8c0cc46038c330a3e
Ronald Otieno <ronnlats@gmail.com>
2021-12-20 19:50:27 +0300

    Update About.vue

--- components/About.vue
+++ components/About.vue
@@ -3,7 +3,7 @@
     class="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20"
   >
     <div>
-    <img src="/lat2-smaller.png" alt="Ronald Otieno image" class="float-left mr-2 bg-violet-500 rounded-md w-40 h-40 aspect-[1/2] [clip-path:circle(70%_at_20%_30%)] [shape-outside:circle(70%_at_20%_30%)]">
+    <img src="/lat2-smaller.webp" alt="Ronald Otieno image" class="float-left mr-2 bg-violet-500 rounded-md w-40 h-40 aspect-[1/2] [clip-path:circle(70%_at_20%_30%)] [shape-outside:circle(70%_at_20%_30%)]">
 
     <p class="first-letter:text-violet-500 first-letter:text-8xl first-letter:italic first-letter:font-[Coco]">
     I am a programmer who specialises in web based projects, having built website and web applications that have an impact on peoples lives. I also have a passion for business and love working with business owners to produce work that is profitable. Clean, minimalistic and functional design is the nature of my work in a nutshell. I believe the main aim of any design should be to guide users to their intended destination, not to confuse them.
